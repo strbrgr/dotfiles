0fb03982ea5d3582a11a7d49fc25a9f86de55615
Jochen Stierberger <jochen@stierberger.com>
2023-01-25 20:05:25 -0600

    update prev and next tab keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,3 +14,7 @@ vim.keymap.set("n", "<leader>ob", ":ObsidianBacklinks<CR>")
 vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>")
 vim.keymap.set("n", "<leader>om", ":ObsidianLink<CR>")
 vim.keymap.set("n", "<leader>ol", ":ObsidianLinkNew<CR>")
+
+-- Tabs Keymaps
+vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
