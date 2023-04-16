4ef13d8a53424242a00ed252669dfa5449a27852
Jochen Stierberger <jochen@stierberger.com>
2023-04-16 14:23:44 -0500

    remove plugins

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,17 +4,6 @@
 
 vim.keymap.set("n", "<leader>md", ":MarkdownPreview<CR>")
 
--- Obsidian Keymaps
-vim.keymap.set("n", "<leader>ot", ":ObsidianToday<CR>")
-vim.keymap.set("n", "<leader>oy", ":ObsidianYesterday<CR>")
-vim.keymap.set("n", "<leader>of", ":ObsidianFollowLinkCR>")
-vim.keymap.set("n", "<leader>os", ":ObsidianSearch<CR>")
-vim.keymap.set("n", "<leader>oo", ":ObsidianOpen<CR>")
-vim.keymap.set("n", "<leader>ob", ":ObsidianBacklinks<CR>")
-vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>")
-vim.keymap.set("n", "<leader>om", ":ObsidianLink<CR>")
-vim.keymap.set("n", "<leader>ol", ":ObsidianLinkNew<CR>")
-
 -- Tabs Keymaps
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
