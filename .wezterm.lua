20ba7f657036dea8e23cfa7569146decf85872a3
Jochen Stierberger <jochen@stierberger.com>
2023-08-01 07:53:32 -0500

    Change Font Size and Weight

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("JetBrainsMono Nerd Font"),
-	font_size = 15,
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
+	font_size = 16,
 	warn_about_missing_glyphs = false,
 	enable_tab_bar = false,
 	color_scheme = "Gruvbox dark, pale (base16)",
