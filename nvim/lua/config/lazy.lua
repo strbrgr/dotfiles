64337155da00725fdbf8bae2d2fddd285e1ce23e
Jochen Stierberger <jochen@stierberger.com>
2024-02-06 15:08:45 -0600

    add go

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.formatting.conform" },
