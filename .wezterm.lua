a42e626ab92ca722085b96d58bacf6c7fa5158ad
Jochen Stierberger <jocsti@web.de>
2024-12-13 13:14:54 -0600

    fix: show tabbar

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ return {
 	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
 	font_size = 16,
 	warn_about_missing_glyphs = false,
-	enable_tab_bar = false,
+	-- enable_tab_bar = false,
 	color_scheme = "Gruvbox dark, pale (base16)",
 	window_decorations = "RESIZE",
 }
