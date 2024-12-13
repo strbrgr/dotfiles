6a470cb47371d5d95371e75c9f159a9f6720af23
Jochen Stierberger <jocsti@web.de>
2024-12-13 17:41:18 -0600

    fix: a bunch of moving and renaming

---
+++ fish/.config/fish/alias.fish
@@ -0,0 +1,11 @@
+# ~/.config/fish/alias.fish
+
+# Alias for `..` to go up one directory
+function ..
+    cd ..
+end
+
+# Alias for `n` to open Neovim in the current directory
+function n
+    nvim .
+end
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,4 @@
+source ~/.config/fish/alias.fish
+
+# Disable new user greeting.
+set fish_greeting
