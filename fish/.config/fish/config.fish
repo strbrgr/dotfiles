b0ebc007f649f2834eeb5cbc39d5a54dd9769836
Jochen Stierberger <blitz@mbp14.local>
2024-12-26 19:13:20 -0600

    feat: Add Flamegraph

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,5 +7,8 @@ set fish_greeting
 # Source cargo
 source "$HOME/.cargo/env.fish"
 
+## Flamgraph autocompletions 
+#flamegraph --completions fish >~/.config/fish/completions
+
 # Source node
 fish_add_path /opt/homebrew/opt/node@22/bin
