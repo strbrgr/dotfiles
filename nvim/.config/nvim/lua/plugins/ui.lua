5f2a9c5b8ff7b777723de64e78b9e42fd2e80290
Jochen Stierberger <jocsti@web.de>
2024-12-26 19:34:41 -0600

    feat: Update ghossty config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,13 @@
 return {
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {
-    contrast = "hard",
-  } },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      contrast = "hard",
+      transparent_mode = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
