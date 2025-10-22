3922041fba800305698620fa2e3b56f5ac781cb9
strbrgr <jocsti@web.de>
2025-10-22 10:24:34 -0500

    add git alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,18 @@
 # Source alias'
 source ~/.config/fish/alias.fish
 
+# Git alias shortcuts (Fish aliases)
+alias ga='git add'
+alias gcm='git checkout main'
+alias gc-='git checkout -'
+alias gaa='git add --all'
+alias gcb='git checkout -b'
+alias gco='git checkout'
+alias gll='git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
+alias gld='git log --pretty=format:"%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short --graph'
+alias gls='git log --pretty=format:"%C(green)%h\\ %C(yellow)[%ad]%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=relative'
+alias gst='git status --short --branch'
+
 # Disable new user greeting.
 set fish_greeting
 
@@ -20,9 +32,9 @@ fish_add_path /opt/homebrew/opt/node@22/bin
 set -gx PATH $PATH /Users/blitz/.lmstudio/bin
 
 # pnpm
-set -gx PNPM_HOME "/Users/blitz/Library/pnpm"
+set -gx PNPM_HOME /Users/blitz/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 chruby ruby-3.4.1
