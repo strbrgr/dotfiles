4445716125be69144dbdb5c52626ae23e5f307d6
strbrgr <jocsti@web.de>
2025-10-30 10:11:44 -0500

    add background blur and change opacity

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
 
-return {
-	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
-	font_size = 16,
-	warn_about_missing_glyphs = false,
-	-- enable_tab_bar = false,
-	color_scheme = "Gruvbox dark, pale (base16)",
-	window_decorations = "RESIZE",
-}
+local config = wezterm.config_builder()
+config.color_scheme = "Gruvbox dark, pale (base16)"
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+config.font_size = 16
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+
+return config
