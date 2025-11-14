49bfe457fd4c9dcd44d6390e56d4ba4326b673ca
strbrgr <jocsti@web.de>
2025-11-14 15:51:50 -0600

    Remove alias file

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,7 @@
-# Source alias'
-source ~/.config/fish/alias.fish
+set -gx EDITOR nvim
 
-# Git alias shortcuts (Fish aliases)
-alias ga='git add'
-alias gcm='git checkout main'
-alias gc-='git checkout -'
-alias gaa='git add --all'
-alias gcb='git checkout -b'
-alias gco='git checkout'
-alias gll='git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
-alias gld='git log --pretty=format:"%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short --graph'
-alias gls='git log --pretty=format:"%C(green)%h\\ %C(yellow)[%ad]%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=relative'
-alias gst='git status --short --branch'
+# Set the path to binaries
+set -gx PATH /opt/homebrew/bin $PATH
 
 # Disable new user greeting.
 set fish_greeting
@@ -21,9 +11,6 @@ source "$HOME/.cargo/env.fish"
 
 # rust analyzer
 set -Ux PATH $HOME/.local/bin $PATH
-
-## Flamgraph autocompletions 
-#flamegraph --completions fish >~/.config/fish/completions
 
 # Source node
 fish_add_path /opt/homebrew/opt/node@22/bin
@@ -37,4 +24,7 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-chruby ruby-3.4.1
+
+# Alias'
+alias n="nvim ."
+alias c="clear"
